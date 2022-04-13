ec7dfa81bbe6dd83a4279318069ded0ca1032267
xugo <xx@golang.space>
2022-04-13 18:38:41 +0800

    字符串遍历

--- base/slice/slice_test.go
+++ base/slice/slice_test.go
@@ -1,6 +1,7 @@
 package slice
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -30,4 +31,47 @@ func nilSliceF() []string {
 }
 func emptySliceF() []string {
 	return make([]string, 0)
+}
+
+// TestMakeSlice 从切片获取切片
+// 共用同一个数组
+func TestMakeSlice(t *testing.T) {
+	s1 := make([]byte, 5, 8)
+	s1[0] = 'a'
+	s1[1] = 'b'
+	s1[2] = 'c'
+	s1[3] = 'd'
+	s1[4] = 'e'
+	require.EqualValues(t, len(s1), 5)
+	require.EqualValues(t, cap(s1), 8)
+
+	s2 := s1[2 : 2+2]
+	require.EqualValues(t, len(s2), 2)
+	require.EqualValues(t, cap(s2), 8-2)
+	inspectSlice("slice1", s1)
+	inspectSlice("slice2", s2)
+	require.EqualValues(t, &s1[2], &s2[0])
+	require.EqualValues(t, &s1[3], &s2[1])
+
+	// s2 = append(s2, 'g')
+	// s2 创建的切片，使用 append 会有副作用
+	// 对 s2 添加元素，导致 s1 也存在同样的函数，这是理所当然的。
+	// 因为共用 同一个数组
+
+	// 无副作用使用 append 方法 1 三下标创建切片
+	s3 := s1[2:4:4]
+	s3 = append(s3, 'f')
+	require.EqualValues(t, len(s1), 5)
+	require.EqualValues(t, len(s3), 3)
+	// 无副作用使用 append 方法 2 copy
+	s4 := make([]byte, len(s1))
+	copy(s4, s1)
+
+}
+
+func inspectSlice(name string, arr []byte) {
+	fmt.Printf("%s length[%d]\tcapacity[%d]\n", name, len(arr), cap(arr))
+	for i := range arr {
+		fmt.Printf("[%d] %p\n", i, &arr[i])
+	}
 }
--- base/string/string_test.go
+++ base/string/string_test.go
@@ -5,11 +5,26 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"unicode/utf8"
 )
 
 const (
 	COUNT = 1000
 )
+
+func TestForRangeStr(t *testing.T) {
+	s := "世界 Hello world"
+
+	var buf [4]byte
+	for i, v := range s {
+		rl := utf8.RuneLen(v)
+		si := i + rl
+		// 字符串是不可变的，所以只能作为来源
+		// 数组也是不可变的，可以通过转为切片，来操作底层数组。
+		copy(buf[:], s[i:si])
+		fmt.Printf("%2d: %q; codepoint: %#6x; encoded bytes: %#v\n", i, v, v, buf[:rl])
+	}
+}
 
 // 通过比较发现
 // builder > buffer > add > sprintf
